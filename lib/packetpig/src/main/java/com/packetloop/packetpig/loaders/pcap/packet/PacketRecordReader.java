090af140945425e092fbed1286ef532442f4bcde
David Turnbull <david@broodax.net>
2012-12-03 08:49:12 +1100

    Removed silly debug line

--- lib/packetpig/src/main/java/com/packetloop/packetpig/loaders/pcap/packet/PacketRecordReader.java
+++ lib/packetpig/src/main/java/com/packetloop/packetpig/loaders/pcap/packet/PacketRecordReader.java
@@ -121,7 +121,6 @@ public class PacketRecordReader extends PcapRecordReader {
 
         PcapPacket packet;
 
-        System.err.println("hello");
         try {
             packet = is.getPacket();
         } catch (NegativeArraySizeException ignored) {
